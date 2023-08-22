5672734833cdd333ab89b2198c0f4b1d1b4174e8
Dennis <mr.dennisgenelazo@gmail.com>
2023-08-22 08:09:35 +0800

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Connect ro DB and start server
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.loge(err));
